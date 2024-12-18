addec95a49e19d2ef4feab2d2ed8cd9283014dc3
Crans <108697172+0xcrans@users.noreply.github.com>
2024-12-18 13:35:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: '*' }
+        ],
+      }
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
