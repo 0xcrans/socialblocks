617ed8769b5d6d544f299d4e7698ce50352bac81
Crans <108697172+0xcrans@users.noreply.github.com>
2024-12-18 13:56:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const nextConfig = {
-  async headers() {
+  async rewrites() {
     return [
       {
         source: '/api/:path*',
+        destination: 'https://api.near.social/:path*'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
