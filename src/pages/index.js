481a1b9774d1c857e9ef5fefeede91ce478ded04
Crans <108697172+0xcrans@users.noreply.github.com>
2024-12-18 14:07:25 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 // pages/index.js
+import Head from 'next/head';
 import SocialFeed from '../components/SocialFeed';
 
 export default function Home() {
-  return (
-    <div>
-      <SocialFeed />
-    </div>
-  );
+ return (
+   <div>
+     <Head>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+       <title>Social Blocks</title>
+     </Head>
+     <SocialFeed />
+   </div>
+ );
 }
